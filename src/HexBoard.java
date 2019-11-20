eec770a74ee2934f4238fc8971a055dd0f240869
Matt Dean <matthew_dean@taylor.edu>
2019-11-20 10:19:06 -0500

    Initial commit

---
+++ src/HexBoard.java
@@ -0,0 +1,35 @@
+/****************************************************************************
+ *  This class manages an N-by-N hex game board .
+ ****************************************************************************/
+
+public class HexBoard {
+    public HexBoard(int N) {
+    }
+
+    public int getPlayer(int row, int col) {
+        return 0;
+    }
+    
+    public boolean isSet(int row, int col) {
+        return false;
+    }
+
+    public boolean isOnWinningPath(int row, int col) {
+        return false;
+    }
+
+    public void setTile(int row, int col, int player) {
+    }
+
+    public boolean hasPlayer1Won() {
+        return false;
+    }
+
+    public boolean hasPlayer2Won() {
+        return false;
+    }
+
+    public int numberOfUnsetTiles() {
+        return 0;
+    }
+}
---
+++ src/HexBoardStats.java
@@ -0,0 +1,13 @@
+/****************************************************************************
+ *  Command: HexBoardStats N0 N1 T
+ *
+ *  This program takes the board sizes N0,N1 and game count T as a command-line
+ *  arguments. Then, the program runs T games for each board size N where
+ *  N0 <= N <= N1 and where each play randomly chooses an unset tile to set in
+ *  order to estimate the probability that player 1 will win.
+ ****************************************************************************/
+
+public class HexBoardStats {
+    public static void main(String[] args) {
+    }
+}
